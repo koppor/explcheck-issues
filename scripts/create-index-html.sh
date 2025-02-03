ee9e84f529657e4f3d6f7caaa6a7974a5b868b92
Oliver Kopp <kopp.dev@gmail.com>
2025-02-03 09:49:21 +0100

    Add Home link

--- scripts/create-index-html.sh
+++ scripts/create-index-html.sh
@@ -21,6 +21,13 @@ generate_index() {
   done
   css_path+="node_modules/bootstrap/dist/css/bootstrap.min.css"
 
+  # Add Home link
+  local home_path=""
+  for (( i=0; i<=$depth; i++ )); do
+    home_path+="../"
+  done
+  breadcrumb+=$'<li class="breadcrumb-item"><a href="'$home_path'">Home</a></li>\n'
+
   for (( i=0; i<${#PARTS[@]}; i++ )); do
     part="${PARTS[$i]}"
     if [ -n "$part" ]; then
