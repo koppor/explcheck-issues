ebf86980dfd2197a3ece69ae36704f6f4936bd19
Oliver Kopp <kopp.dev@gmail.com>
2025-02-03 09:41:48 +0100

    Fix breadcrumps

--- scripts/create-dirs-for-files.sh
+++ scripts/create-dirs-for-files.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Creates a directory with index.html, the file itself, errors.txt, errors.json for browsing
+
 set -e
 
 jq -r '.[].filename' < errors.json | sed 's/\r$//' | sort -u | while read -r filepath; do
@@ -28,28 +30,37 @@ jq -r '.[].filename' < errors.json | sed 's/\r$//' | sort -u | while read -r fil
   done
   sed -i "s#../node_modules#../${repeat_up}node_modules#" "publish/$dir_path/index.html"
   sed -i "s#./latex.json#../${repeat_up}latex.json#" "publish/$dir_path/index.html"
-  sed -i "s#HOME#../${repeat_up}#" "publish/$dir_path/index.html"
 
   sed -i "s/e102.lua/$(basename $dir_path)/" "publish/$dir_path/index.html"
 
   # Split path into an array using '/' as a delimiter
   IFS='/' read -r -a path_parts <<< "$dir_path"
   breadcrumb_html=""
-  link_prefix=""
-  for ((i=0; i<${#path_parts[@]}; i++)); do
+
+  # Determine how many levels deep we are to set the base link correctly
+  depth=${#path_parts[@]}
+
+  # Add Home link pointing to the root
+  breadcrumb_html+="<li class=\"breadcrumb-item\"><a href=\"$(printf '../%.0s' $(seq 1 $depth))\">Home</a></li>\n"
+
+  # Build breadcrumbs for each part
+  for ((i=0; i<depth; i++)); do
     part="${path_parts[$i]}"
 
-    # Build the correct link based on current depth
-    link="$link_prefix"
+    # If it's the last part, link to '#', else calculate relative path
+    if [[ $i -eq $((depth - 1)) ]]; then
+      link="#"
+    else
+      up_levels=$((depth - i - 1))
+      link="$(printf '../%.0s' $(seq 1 $up_levels))"
+    fi
 
     # Append the breadcrumb item
     breadcrumb_html+="<li class=\"breadcrumb-item\"><a href=\"$link\">$part</a></li>\n"
-
-    # Update link_prefix to go one level deeper
-    link_prefix+="${part}/"
   done
 
-  sed -i "s#NAV#$breadcrumb_html#" "publish/$dir_path/index.html"
+  # Replace NAV placeholder in the target HTML file
+  sed -i "s!NAV!${breadcrumb_html}!" "publish/$dir_path/index.html"
       
   if [[ "$dir_path" == *tex ]]; then
     sed -i "s/'lua'/'latex'/" "publish/$dir_path/index.html"
