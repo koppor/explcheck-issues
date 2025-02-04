ac9e51b5f4a119519ce1cdbbf29a25eb6c307b19
Oliver Kopp <kopp.dev@gmail.com>
2025-02-04 22:25:21 +0100

    Try to fix path depth

--- scripts/create-dirs-for-files.sh
+++ scripts/create-dirs-for-files.sh
@@ -9,30 +9,32 @@ jq -r '.[].filename' < errors.json | sed 's/\r$//' | sort -u | while read -r fil
 
   # We use the filename of the errored file as directory name
   # In that directory, more files (errors.json, ...) will be placed
-  dir_path=$filepath
 
-  # Create directory structure
-  mkdir -p "publish/$dir_path"
+  target_path=${filepath#/tmp/texlive/usr/local/texlive/[0-9][0-9][0-9][0-9]/}
 
-  echo $filtered_errors > "publish/$dir_path/errors.json"
-  cp $dir_path "publish/$dir_path/"
-  grep $filepath errors.txt | sort -V > "publish/$dir_path/errors.txt"
+  mkdir -p "publish/$target_path"
+
+  echo $filtered_errors > "publish/$target_path/errors.json"
+  cp $filepath "publish/$target_path/"
+  grep $filepath errors.txt | sort -V > "publish/$target_path/errors.txt"
 
   # Copy index.html to the new directory (rename it to match filename if needed)
-  cp pages/file/index.html "publish/$dir_path/index.html"
+  cp pages/file/index.html "publish/$target_path/index.html"
 
-  dir_levels=$(awk -F'/' '{print NF-1}' <<< "$dir_path")
+  # TODO: This can be replaced by the "Split path" logic below
+  dir_levels=$(awk -F'/' '{print NF-1}' <<< "$target_path")
   repeat_up=""
   for ((i=0; i<dir_levels; i++)); do
     repeat_up+="../"
   done
-  sed -i "s#../node_modules#../${repeat_up}node_modules#" "publish/$dir_path/index.html"
-  sed -i "s#./latex.json#../${repeat_up}latex.json#" "publish/$dir_path/index.html"
 
-  sed -i "s/e102.tex/$(basename $dir_path)/" "publish/$dir_path/index.html"
+  sed -i "s#../node_modules#../${repeat_up}node_modules#" "publish/$target_path/index.html"
+  sed -i "s#./latex.json#../${repeat_up}latex.json#" "publish/$target_path/index.html"
+
+  sed -i "s/e102.tex/$(basename $target_path)/" "publish/$target_path/index.html"
 
   # Split path into an array using '/' as a delimiter
-  IFS='/' read -r -a path_parts <<< "$dir_path"
+  IFS='/' read -r -a path_parts <<< "$target_path"
   breadcrumb_html=""
 
   # Determine how many levels deep we are to set the base link correctly
@@ -58,9 +60,9 @@ jq -r '.[].filename' < errors.json | sed 's/\r$//' | sort -u | while read -r fil
   done
 
   # Replace NAV placeholder in the target HTML file
-  sed -i "s!NAV!${breadcrumb_html}!" "publish/$dir_path/index.html"
+  sed -i "s!NAV!${breadcrumb_html}!" "publish/$target_path/index.html"
       
-  if [[ "$dir_path" == *lua ]]; then
-    sed -i "s/'latex'/'lua'/" "publish/$dir_path/index.html"
+  if [[ "$target_path" == *lua ]]; then
+    sed -i "s/'latex'/'lua'/" "publish/$target_path/index.html"
   fi
 done
